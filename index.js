24ebcae95edc28252f268914233ff7d21122d950
nishant parashar <nnishant.parashar@gmail.com>
2023-11-15 17:34:07 +0530

    CORS added

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const {db} = require("./db/connect");
 const userRoutes = require("./routes/userRoute");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const PORT = 8000;
+app.use(cors());
 app.use(express.json());
 app.use("/v1",userRoutes);
 
