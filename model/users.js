c5c3b60f4cfd59f00c9618231dd09c673512d47b
nishant parashar <nnishant.parashar@gmail.com>
2023-11-15 17:59:12 +0530

    user model updated

--- model/users.js
+++ model/users.js
@@ -2,27 +2,15 @@ const mongoose = require("mongoose");
 
 const usersSchema = new mongoose.Schema(
     {
-        id: {type: Number, unique:true, required:true, trim: true},
+        
         name: {type: String, required:true, trim: true},
         username: {type: String, required:true, trim: true},
         email: {type: String, unique:true, required:true, trim: true},
-        address: {
-        street: {type: String, required:true, trim: true},
-        suite: {type: String, required:true, trim: true},
-        city: {type: String, required:true, trim: true},
-        zipcode: {type: String, required:true, trim: true},
-        geo: {
-            lat: {type: String, required:true, trim: true},
-            lng: {type: String, required:true, trim: true}
-        }
-        },
+        address: {type: String, required:true, trim: true},
         phone: {type: String, unique:true, required:true, trim: true},
         website: {type: String, required:true, trim: true},
-        company: {
-        name: {type: String, required:true, trim: true},
-        catchPhrase: {type: String, required:true, trim: true},
-        bs: {type: String, required:true, trim: true}
-        }
+        company: {type: String, required:true, trim: true},
+        
     }
 );
 
